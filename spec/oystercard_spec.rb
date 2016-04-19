71a3b0f44e691ef912e482c4756b2f779e4b3517
Hanna <hannaeberhoefer@yahoo.com>
2016-04-19 09:29:54 +0100

    Correction

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,20 +1,21 @@
 require 'oystercard'
 
-describe Oystercard do 
+describe Oystercard do
 
-	describe "#initalize" do
-		it "has a default balance of £0" do
-			expect(subject.balance).to eq 0
+	describe '#initalize' do
+		it 'has a default balance of £0' do
+			expect(subject.balance).to eq(0)
 	  end
 	end
 
-	describe "#top_up" do
-		it "allows putting money on oystercard" do
-			expect(Oystercard.new.top_up(3)).to eq 3
+	describe '#top_up' do
+		it 'allows putting money on oystercard' do
+			expect{subject.top_up(3)}.to change{subject.balance}.by(3)
 		end
 		it "can only top up to a maximum of £90" do
-			subject.top_up(90)
-			expect{subject.top_up(6)}.to raise_error "Can't top up over £90"
+			max_balance = Oystercard::MAX_BALANCE
+			subject.top_up(max_balance)
+			expect{subject.top_up(6)}.to raise_error "Can't top up over £#{max_balance}"
 		end
 	end
 
